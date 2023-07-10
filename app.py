3d1fb31ea39817771bac8a6bfe8e5c34d6de4c61
jdmmctrl <juandavidmurillomejiaoutlook.es>
2023-07-09 22:12:35 -0500

    Actualizacion 09/07/2023/00

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def inicio():
+    return render_template("sitio/index.html")
+
+
+@app.route("/libros")
+def libros():
+    return render_template("sitio/libros.html")
+
+
+@app.route("/nosotros")
+def nosotros():
+    return render_template("sitio/nosotros.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
